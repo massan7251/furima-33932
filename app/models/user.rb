70e476cb18c35b99039330646f34448df1be1c17
massan7251 <kurafgo9999@gmail.com>
2021-02-09 18:58:32 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
