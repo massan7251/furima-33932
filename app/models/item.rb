91c15d8c1297b1264d2b2d427d53b339fc9e2335
massan7251 <kurafgo9999@gmail.com>
2021-02-10 21:19:59 +0900

    ActiveHashの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,21 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :shipment
+
+ with_options presence: true do
+  validates :image
+ with_options numericality: { other_than: 1 } do
+  validates :category
+  validates :status
+  validates :burden
+  validates :prefecture
+  validates :shipment
+ end
+ end
 end
