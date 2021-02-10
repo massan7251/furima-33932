7ab8ffb334e101673f5f0aa4d4183aa1c2670765
massan7251 <kurafgo9999@gmail.com>
2021-02-10 18:12:40 +0900

    本名カラムに全角のみ可バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   validates :name, length: { maximum: 40 }
   validates :email, uniqueness: true
   validates :encrypted_password
-  validates :first_name
-  validates :last_name
-  validates :first_name_kana, format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/ }
-  validates :last_name_kana, format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/}
+  validates :first_name, format: { with: ~/^[^ -~｡-ﾟ]*$/ }
+  validates :last_name, format: { with: ~/^[^ -~｡-ﾟ]*$/ }
+  validates :first_name_kana, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ }
+  validates :last_name_kana, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/  }
   validates :birthday, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
