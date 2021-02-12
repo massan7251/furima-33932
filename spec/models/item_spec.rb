83d566ee92a69b6a808f67ad927b618f074733bf
massan7251 <kurafgo9999@gmail.com>
2021-02-12 10:15:20 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,15 +16,15 @@ RSpec.describe Item, type: :model do
       expect(@item).to be_valid
     end
     it 'priceが300以上であれば登録できる' do
-      @item.price = '300'
+      @item.price = 300
       expect(@item).to be_valid
     end
     it 'priceが9999999以下であれば登録できる' do
-      @item.price = '9999999'
+      @item.price = 9999999
       expect(@item).to be_valid
     end
     it 'priceが半角数字であれば登録できる' do
-      @item.price = '5555555'
+      @item.price = 5555555
       expect(@item).to be_valid
     end
    end
@@ -50,27 +50,27 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Price can't be blank")
     end
     it 'category_idが1であれば登録できない' do
-      @item.category_id = '1'
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category must be other than 1")
     end
     it 'status_idが1であれば登録できない' do
-      @item.status_id = '1'
+      @item.status_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Status must be other than 1")
     end
     it 'burden_idが1であれば登録できない' do
-      @item.burden_id = '1'
+      @item.burden_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Burden must be other than 1")
     end
     it 'prefecture_idが1であれば登録できない' do
-      @item.prefecture_id = '1'
+      @item.prefecture_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
     end
     it 'shipment_idが1であれば登録できない' do
-      @item.shipment_id = '1'
+      @item.shipment_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipment must be other than 1")
     end
@@ -80,12 +80,12 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Description is too long (maximum is 40 characters)")
     end
     it 'priceがあっても300以下では登録できない' do
-      @item.price = '299'
+      @item.price = 299
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
     end
     it 'priceがあっても9999999以上では登録できない' do
-      @item.price = '10000000'
+      @item.price = 10000000
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
     end
